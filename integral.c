252f5cd6a3ccb98b2dc18913397ebbeab07aac2a
guraybaydur <guraybaydur@gmail.com>
2017-10-19 17:56:38 +0300

    added all functionalities for partA but not tested yet

--- integral.c
+++ integral.c
@@ -5,8 +5,21 @@
 #include <unistd.h>
 
 #include "function.h"
+	
 
-//double
+
+double giveToChild(double v,double subInterval , int k)
+{
+	//double subInterval = (v - u) / ( (double)k ); 
+	double resultOfChild = 0.0;
+	
+	for(int j = 1; j <= k;j++)
+	{
+		resultOfChild = subInterval * ((compute_f(v + (j - 1) * subInterval) + compute_f(v + j * subInterval))) / 2;
+	}
+
+	return resultOfChild;			
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,25 +33,82 @@ int main(int argc, char *argv[])
   	printf ("The l value entered is %d.\n",intL);
   	printf ("The U value entered is %d.\n",intU);
   	printf ("The K value entered is %d.\n",intK);
-  	printf ("The N value entered is %d.\n",intN);
+  	printf ("The N value entered is %d.\n\n",intN);
 	
 
 //	pid_t parent = getpid();
 	double interval = ((double)intU - (double)intL) / (double)intN ;
-	printf("the interval is: %f\n", interval);
+	printf("the interval is: %f\n\n", interval);
+	
+	double overallCalculation = 0.0;
 
 	//Create N child process
 	for(int i = 1; i <= intN; i++)
 	{
-		pid_t child = fork();
-		if(child == 0)
+		printf("going on i:  %d\n\n",i);
+
+		int fileDes[2];
+
+		if(pipe(fileDes)== -1)
 		{
+			printf("pipe creation failed\n");
+		}
+		else 
+		{
+			printf("pipe creation successful\n");
+		}
+
+		pid_t child = fork();
+		
+		if(child == 0)//if child is created
+		{
+			double childsResult = 0.0;
+
+			printf("Child Created\n");
+			
+			if(close(fileDes[0]) == 0)
+			{	
+				printf("Read end of child closed\n\n");
+			}
+			else{printf("error while closing the child\n\n");}
+			
+			//compute the subinterval specific for child
+			double subInterval = (intL + ((double)(i))*(interval)) - (intL + ((double)(i-1))*(interval));							      printf("subInterval is : %f \n\n", subInterval);			
+
 			//call child process function 
-			//giveToChild( (intL + ((double)(i-1))*(interval)),  (intL + ((double)(i))*(interval)),intK);
+			childsResult = giveToChild( (intL + ((double)(i-1))*(interval)), subInterval ,intK);
+			printf("result of child %d is: %f\n", i ,childsResult);
+			int res = write(filedes[1],&childsResult,sizeof(childsResult));
+
+			//write(send) the child's result to parent
+			if(res == -1)
+			{
+				printf("write operation failed \n");
+			}
+			else{printf("successfully written!!! Num of bytes: %d\n",res );}
 			
 			exit(0);	
-		} 
-		printf("going on\n");
+		}
+		else if(child < 0)
+		{
+			printf("Child creation failed\n");
+			exit(-1);
+		}
+		else //if no error exists and in parent
+		{
+			close(fileDes[1]);
+			//Close write end
+			for(int p = 1; p < intN; p++)
+			{
+				double y = 0.0;
+				double *x = &y;
+				wait();
+				read(fileDes[0],&x, sizeOf(double));
+				overallCalculation = overallCalculation + *x;	
+			}
+			
+			printf("Overall Calculation: %f\n\n", overallCalculation);			
+		}
 	}  
 
 	return 0;
